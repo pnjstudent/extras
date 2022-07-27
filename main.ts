7d816f68e427a670ed02c347a9a71c6e70640010
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-07-27 05:53:28 -0400

    adding sound blocks

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-namespace algorithms {
+//% color="#2b2c6e" icon="\uf067"
+namespace extras {
     /**
      * Simple moving average
      */
     //% group="Filters"
-    //% blockId=algosma
     //% block="smoothen value $input with $points points"
     export function smoothenValue(input: number, points: number): number {
         const values: number[] = [];
@@ -24,12 +24,34 @@ namespace algorithms {
      * Shuffles arrays
      */
     //% group="Arrays"
-    //% blockId=algosshufflearr
-    //% block="shuffle array $array=variables_get(list)"
+    //% block="shuffle array $array"
+    //% array.shadow="lists_create_with"
     export function shuffleArray(array: any[]): void {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
     }
+
+    let maxVol = 0;
+
+    //% group="Sound"
+    //% block="map $imgs to the sound level"
+    //% imgs.shadow="lists_create_with"
+    export function mapImagesToVolume(imgs: Image[]) {
+        maxVol = Math.max(maxVol, input.soundLevel());
+        const getAmplifiedVol = () => input.soundLevel() / maxVol * 255;
+        const step = 255 / imgs.length;
+        imgs.length && imgs[Math.clamp(0, imgs.length - 1, Math.round(getAmplifiedVol() / step))].showImage(0);
+    }
+
+    /**
+     * Reset Max Volume Level Detected
+     */
+    //% group="Sound"
+    //% block="reset max volume detected"
+    export function resetMaxVol() {
+        maxVol = 0;
+    }
 }
+
