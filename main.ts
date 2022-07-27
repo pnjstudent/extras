a6316f794351154e68bba15b2d9656cc48187e2e
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-07-27 01:53:34 -0400

    adding groups and variable shadows

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ namespace algorithms {
     /**
      * Simple moving average
      */
+    //% group="Filters"
     //% blockId=algosma
-    //% block="smoothen value $v with $p points"
+    //% block="smoothen value $input with $points points"
     export function smoothenValue(input: number, points: number): number {
         const values: number[] = [];
         const avg = () => values.reduce((prev: number, curr: number) => prev + curr, 0) / points;
@@ -17,6 +18,18 @@ namespace algorithms {
             values.shift();
             values.push(input);
             return avg();
+        }
+    }
+    /**
+     * Shuffles arrays
+     */
+    //% group="Arrays"
+    //% blockId=algosshufflearr
+    //% block="shuffle array $array=variables_get(list)"
+    export function shuffleArray(array: any[]): void {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
 }
